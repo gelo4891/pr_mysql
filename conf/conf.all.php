dc06573f244e5a9dadefb02fc8f6bfb1c6223866
gelo4891 <https://github.com/gelo4891/pr_mysql.git>
2019-05-13 17:05:29 +0300

    conf

---
+++ conf/conf.all.php
@@ -0,0 +1,21 @@
+﻿<?php
+unset ($arr_conf_list);
+
+    $arr_conf_list[]=array(
+        "id_param"=>1,
+        "name_host"=>"localhost",
+        "name_base"=>"id9161148_my_test_base",
+        "name_user"=>"id9161148_base_user",
+        "name_pass"=>"Base_user"
+    );
+
+    $arr_conf_list[]=array(
+        "id_param"=>2,
+        "name_host"=>"localhost",
+        "name_base"=>"my_test_base",
+        "name_user"=>"id9161148_base_user",
+        "name_pass"=>"Base_user"
+    );
+
+
+?>
